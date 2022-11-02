a546080d73478a0e0a1ba1d046b0bcd69089fa06
Usman Akram <usman.akram@gmail.com>
2022-11-02 15:14:08 +0500

    server side crud for section b

--- sp20-bcs-b/express-cruds/routes/site/cars.js
+++ sp20-bcs-b/express-cruds/routes/site/cars.js
@@ -1,10 +1,36 @@
 const express = require("express");
-const Router = express.Router();
+const router = express.Router();
 const Car = require("../../models/car");
-Router.get("/cars", async (req, res) => {
+
+router.get("/cars/delete/:id", async (req, res) => {
+  let car = await Car.findByIdAndDelete(req.params.id);
+  res.redirect("/cars");
+});
+router.get("/cars/edit/:id", async (req, res) => {
+  let car = await Car.findById(req.params.id);
+  res.render("cars/edit", { car });
+});
+router.post("/cars/edit/:id", async (req, res) => {
+  let car = await Car.findById(req.params.id);
+  car.model = req.body.model;
+  car.make = req.body.make;
+  car.variant = req.body.variant;
+  await car.save();
+  res.redirect("/cars");
+});
+
+router.get("/cars/add", (req, res) => {
+  res.render("cars/add");
+});
+router.post("/cars/add", async (req, res) => {
+  let car = new Car(req.body);
+  await car.save();
+  res.redirect("/cars");
+});
+router.get("/cars", async (req, res) => {
   const pageTitle = "List of Cars";
   const cars = await Car.find();
   res.render("cars/list", { pageTitle, cars });
 });
 
-module.exports = Router;
+module.exports = router;
--- sp20-bcs-b/express-cruds/server.js
+++ sp20-bcs-b/express-cruds/server.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 var expressLayouts = require("express-ejs-layouts");
-
 const app = express();
-
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 app.use(expressLayouts);
-
 //add a router to our app
 app.use("/", require("./routes/site/cars"));
 app.get("/", (req, res) => {
